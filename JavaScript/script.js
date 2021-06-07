27bc5568ba4265bc7141be206f0d9a5f1a451f19
Mateus <mateussilvafolego26@gmail.com>
2021-06-07 18:18:28 -0300

    HTML-CSS-JS-finished

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,50 +1,66 @@
-
 let tasks = [];
 
 function idGenerator(){
 
-    return Math.floor(Math.random() * 1000)
+    return Math.floor(Math.random() * 9000);
 };
 
 function creatMyTask(){
     let taskName = document.getElementById('inputStyle').value;
-
+    
     let task = {
         id: idGenerator(),
         name: taskName
     };
 
-tasks.push(task);
+    if(taskName == '' || !taskName.trim()){
+        return alert('please, enter a valid name for your task!');
+    }
 
-addLocalStorage(tasks);
+    else{
+    tasks.push(task);
+    addLocalStorage(tasks);
+    };
+};
 
+{const inputStyle = document.getElementById('inputStyle');
+inputStyle.addEventListener('keydown', function(event){
+    if (event.key == "Enter") {
+        creatMyTask();
+    };
+});
 };
 
 function updateScreen(){
-    let list = '<ul>';
+    let list = '<ul>'
 
     tasks.forEach(task => {
-        list += '<li id='+task.id+'>'+task.name+'<button class="action" onclick=deleteTask(this) id= '+task.id+'><i class="fa fa-trash"></i></button></li>';
+        list += '<li id=' + task.id + '><h3 id="nameTask">' + task.name + '</h3><div><button class="action" onclick=deleteTask(this) id= ' + task.id + '><i class="fa fa-trash"></i></button></div></li>';
     });
+    list += '</ul>';
 
-    list += "</ul>";
-    
 document.getElementById('tasksList').innerHTML = list;
 document.getElementById('inputStyle').value = '';
 
 };
 
 function deleteTask(element){
+    let confirmation = window.confirm('Are you sure you want to delete this task?');
+
+    if(confirmation){
     tasks = tasks.filter(task=>task.id!=element.getAttribute('id'));
- 
+    };
     addLocalStorage(tasks);
 
 };
 
 function cleanAll(){
+    let confirmation = window.confirm('Are you sure you want to delete all tasks?');
+    if (confirmation){
     tasks = [];
+    };
     addLocalStorage(tasks);
-}
+};
 
 function addLocalStorage(tasks){
     localStorage.setItem('tasks', JSON.stringify(tasks));
@@ -57,9 +73,7 @@ function getFromLocalStorage(){
 
     if(tasksload){
         tasks = JSON.parse(tasksload);
-
         updateScreen();
-
     };  
 };
 
